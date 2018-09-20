4527c2ead762042dd8a1f5dbd20244b38a10adc8
lukeherczeg <lukeherczeg@ufl.edu>
2018-09-19 20:33:44 -0400

    Final

--- src/linkededitor.cpp
+++ src/linkededitor.cpp
@@ -25,8 +25,8 @@ template <class T> void LinkedList<T>::insert(int index,T value){
 		return;
 	}
 	Node <T> * current = head;
-	for(int i = 0; i < index - 2; ++i){
-		if(current->next == NULL)
+	for(int i = 1; i < index - 1; ++i){ // For the insertion loop, we are fine landing on current = NULL, so we check BEFORE the loop so that if the index is too high, we don't
+		if(current->next == NULL)       // go PAST current = null.
 			return;
 		current = current->next;
 	}
@@ -34,30 +34,32 @@ template <class T> void LinkedList<T>::insert(int index,T value){
 	newNode->data = value;
 	newNode->next = current->next;
 	current->next = newNode;
-	if(current == tail){
+	if(current == tail)
 		tail = newNode;
-	}
 }
 
 template <class T> void LinkedList<T>::remove(int index){
+	Node <T> * current = head;
+	if(current == NULL)     					 // If index given is anything, but the list is empty, we have to check here so delete 1 doesn't break the code.
+		return;
 	if(index == 1){
-		Node <T> * temp = head;
 		head = head->next;
-		delete temp;
-		temp = NULL;
+		delete current;
+		current = NULL;
 		return;
 	}
-	Node <T> * current = head;
-	for(int i = 0; i < index - 2; ++i){
-		current = current->next;
+	if(current->next == NULL) 					 // If index is 2, loop won't run, so have to check here if the size is 1, so delete 2 doesn't break the code.
+		return;
+	for(int i = 1; i < index - 1; ++i){			 // For the deletion loop here, we don't want to have current = NULL. We may be able to insert on NULL but we can't delete on it,
+		current = current->next;				 // hence, we check for NULL after the indexing.
 		if(current->next == NULL)
 			return;
 	}
+
 	Node <T> * temp = current->next;
 	current->next = temp->next;
-	if(temp == tail){
+	if(temp == tail)
 		tail = current;
-	}
 	delete temp;
 	temp = NULL;
 }
@@ -74,8 +76,13 @@ template <class T> void LinkedList<T>::print(){
 
 template <class T> void LinkedList<T>::edit(int index, T value){
 	Node <T> * temp = head;
-	for(int i = 0; i < index-1; i++)
+	if(temp == NULL) 							// Same logic as delete function for handling empty linked list.
+		return;
+	for(int i = 1; i < index - 1; i++){
 		temp = temp->next;
+		if(temp->next == NULL)
+			return;
+	}
 	temp->data = value;
 }
 
@@ -98,21 +105,21 @@ template <class T> void LinkedList<T>::find(string value){
 }
 
 string stringAfterSpace(string word){
-	for(unsigned int i = 0; i < word.size(); i++)
+	for(unsigned int i = 0; i < word.size(); i++) // Finds the string input after a space
 		if (isspace(word[i]))
 			return word.substr(i+1);
 	return "";
 }
 
 string stringBeforeSpace(string word){
-    for(unsigned int i = 0; i < word.size(); i++)
+    for(unsigned int i = 0; i < word.size(); i++) // Finds the string input before a space
         if (isspace(word[i]))
         	return word.substr(0,i);
     return word;
 }
 
 string removeQuotes(string word){
-	return word.substr(1,word.size()-2);
+	return word.substr(1,word.size()-2);		// Simple algorithm to remove quotes to make inputs easier
 }
 
 int toInt(string numStr){
